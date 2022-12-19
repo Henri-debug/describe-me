26273f656b9416b4189d7ff653e91534eb7f1f74
Henrique Liberato <rickbrptcode@hotmail.com>
2022-12-19 16:31:45 -0300

    add routes and modified landing page of 'Home' to 'Landing'

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Home} from './pages/Home/'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from './pages/Home'
+import {Landing} from './pages/Landing'
 import './styles/global.css'
 
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
